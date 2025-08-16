cdba857dd63eac1795f73fb475007f3317016411
Chelshiyaa <gautamchelsea639@gmail.com>
2025-08-16 23:14:10 +0530

    fixed routing issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 // components/Navbar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link'; // Import HashLink
 import { ArrowRightIcon, UserPlusIcon } from './icons';
 
 const Navbar = ({ user, onLogout }) => {
@@ -12,10 +13,10 @@ const Navbar = ({ user, onLogout }) => {
           <span className="text-xl font-bold text-gray-800">ServiceHub</span>
         </Link>
         <div className="hidden md:flex space-x-8">
-          {/* Replace <a> with <Link> for correct routing */}
-          <Link to="/#services" className="text-gray-700 hover:text-blue-600 transition duration-300">Services</Link>
-          <Link to="/#how-it-works" className="text-gray-700 hover:text-blue-600 transition duration-300">How it Works</Link>
-          <Link to="/#about" className="text-gray-700 hover:text-blue-600 transition duration-300">About</Link>
+          {/* Use HashLink for smooth scrolling to sections */}
+          <HashLink to="/#services" className="text-gray-700 hover:text-blue-600 transition duration-300">Services</HashLink>
+          <HashLink to="/#how-it-works" className="text-gray-700 hover:text-blue-600 transition duration-300">How it Works</HashLink>
+          <HashLink to="/#about" className="text-gray-700 hover:text-blue-600 transition duration-300">About</HashLink>
         </div>
         <div className="flex items-center space-x-4">
           {user ? (
